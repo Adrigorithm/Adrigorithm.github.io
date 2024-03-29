df41c7a7019f3496659d5112f40910495ecf9013
Adrigorithm <2ndalgorithm@gmail.com>
2024-03-29 15:04:36 +0100

    Added first project and GH embed :3

--- assets/js/githubEmbedder.js
+++ assets/js/githubEmbedder.js
@@ -1,6 +1,48 @@
+import { WriteMode } from "./enums.js";
+import { DateStringToDate, SetTextContent } from "./utilities.js";
+
 class GithubEmbedder{
     static fromPublicRepoJSON(json){
+        let container = document.createElement("fieldset");
+        let header = document.createElement("legend");
+        let titleLink = document.createElement("a");
+        let title = document.createElement("h3");
+        let ownerLink = document.createElement("a");
+        let owner = document.createElement("h4");
+        let ownerPrefix = document.createElement("span");
+        let description = document.createElement("p");
+        let updated = document.createElement("p");
 
+        let titleText = document.createTextNode(json.name);
+        let legendText = document.createTextNode(json.name);
+        let ownerText = document.createTextNode(json.owner.login);
+        let ownerPrefixText = document.createTextNode("created by ");
+        let descriptionText = document.createTextNode(json.description);
+        let updatedText = document.createTextNode(`Last updated at ${DateStringToDate(json.pushed_at)}`);
+
+        container.classList.add("border-solid", "border-2", "border-zaffre", "text-center", "m-2", "lg:w-full");
+        titleLink.classList.add("no-underline", "text-black", "dark:text-white");
+        ownerLink.classList.add("no-underline", "text-black", "dark:text-white");
+        header.classList.add("text-bold");
+
+        titleLink.setAttribute("href", json.html_url);
+        titleLink.setAttribute("target", "_blank");
+        ownerLink.setAttribute("href", json.owner.html_url);
+        ownerLink.setAttribute("target", "_blank");
+
+        SetTextContent(header, WriteMode.APPEND, legendText);
+        SetTextContent(description, WriteMode.APPEND, descriptionText);
+        SetTextContent(updated, WriteMode.APPEND, updatedText);
+        SetTextContent(title, WriteMode.APPEND, titleText);
+        SetTextContent(ownerPrefix, WriteMode.APPEND, ownerPrefixText);
+        owner.appendChild(ownerPrefix);
+        SetTextContent(owner, WriteMode.APPEND, ownerText);
+
+        titleLink.appendChild(title);
+        ownerLink.appendChild(owner);
+        container.append(header, titleLink, ownerLink, description, updated);
+
+        return container;
     }
 }
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ function PageLoaded() {
     SetupScrollEvent();
     ConstructTimeLines();
     ConstructRecipes();
-    GenereateGithubEmbeds();
+    GenereateGithubEmbeds("https://api.github.com/repos/Adrigorithm/Adribot");
 }
 
 function SetupScrollEvent() {
@@ -34,11 +34,13 @@ function SetupScrollEvent() {
 
 function GenereateGithubEmbeds(...urls) {
     let dataLoader = new DataLoader();
-    let embeds = {};
 
     urls.forEach(url => {
-        dataLoader.FetchData("https://api.github.com/repos/Adrigorithm/Adribot", RequestMethod.GET, { "Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" }).then((data) => {
-            embeds[data.name] = GithubEmbedder.fromPublicRepoJSON(data);
+        dataLoader.FetchData(url, RequestMethod.GET, { "Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" }).then((data) => {
+            let html = GithubEmbedder.fromPublicRepoJSON(data);
+
+            let githubEmbedParent = document.getElementById([data.name]);
+            githubEmbedParent.appendChild(html);
         })
     });
 }
